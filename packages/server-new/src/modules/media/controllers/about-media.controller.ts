86f5398e4465023a049f8097c1142de446edd6b8
zisiszak <126941721+zisiszak@users.noreply.github.com>
2024-09-10 01:03:21 +1000

    feat: about media controller

---
+++ packages/server-new/src/modules/media/controllers/about-media.controller.ts
@@ -0,0 +1,21 @@
+import { type RequestHandler } from 'express';
+import { UserRoleEnum } from '~/modules/user/user-role.js';
+import { Media } from '../Media.js';
+import { type AboutMedia } from '../model.js';
+
+export type Status200_Success = AboutMedia;
+export type Status404_NotFound = never;
+
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+export type ResponseBody = Status200_Success | Status404_NotFound;
+
+export const aboutMediaController: RequestHandler<any, ResponseBody> = async (req, res) => {
+	const aboutMedia = await Media.getAboutMedia(
+		req.mediaID!,
+		req.userAccessToken!.role === UserRoleEnum.Admin ? undefined : req.userAccessToken!.userID,
+	);
+
+	if (!aboutMedia) return void res.status(404).end();
+
+	return res.status(200).json(aboutMedia).end();
+};
